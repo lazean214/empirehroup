ef6612ef2234da9a198a2cd3a7ecc85f9e34a6c4
lazean214 <salazar.neil@gmail.com>
2021-11-07 11:53:21 +0400

    added navbar background animation

--- js/script.js
+++ js/script.js
@@ -1,30 +1,29 @@
-function openNav() {
-    document.getElementById("myNav").style.display = "block";
-  }
+$(window).scroll(function() {
   
-  function closeNav() {
-    document.getElementById("myNav").style.display = "none";
-  }
-
-//   const swiper = new Swiper('.swiper', {
-//     // Optional parameters
-//     direction: 'vertical',
-//     loop: true,
+  // selectors
+  var $window = $(window),
+      $body = $('.navbar'),
+      $panel = $('.section');
   
-//     // If we need pagination
-//     pagination: {
-//       el: '.swiper-pagination',
-//     },
+  // Change 33% earlier than scroll position so colour is there when you arrive.
+  var scroll = $window.scrollTop() + ($window.height() / 3);
+ 
+  $panel.each(function () {
+    var $this = $(this);
+    
+    // if position is within range of this panel.
+    // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+    // Remember we set the scroll to 33% earlier in scroll var.
+    if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+          
+      // Remove all classes on body with color-
+      $body.removeClass(function (index, css) {
+        return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+      });
+       
+      // Add class of currently active div
+      $body.addClass('color-' + $(this).data('color'));
+    }
+  });    
   
-//     // Navigation arrows
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-  
-//     // And if we need scrollbar
-//     scrollbar: {
-//       el: '.swiper-scrollbar',
-//     },
-//   });
-
+}).scroll();
