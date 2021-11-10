ebb00522c80bf369b0ebe89ba04bd9e222fd0020
lazean214 <salazar.neil@gmail.com>
2021-11-10 11:25:21 +0400

    updated carouse text

--- js/script.js
+++ js/script.js
@@ -77,23 +77,23 @@ $(document).ready(function() {
 
 
 // Wrap every letter in a span
-var textWrapper = document.querySelector('.ml6 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+// var textWrapper = document.querySelector('.ml6 .letters');
+// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml6 .letter',
-    translateY: ["1.1em", 0],
-    translateZ: 0,
-    duration: 1000,
-    delay: (el, i) => 50 * i
-  }).add({
-    targets: '.ml6',
-    opacity: 0,
-    duration: 2000,
-    easing: "easeOutExpo",
-    delay: 6000
-  });
+// anime.timeline({loop: true})
+//   .add({
+//     targets: '.ml6 .letter',
+//     translateY: ["1.1em", 0],
+//     translateZ: 0,
+//     duration: 1000,
+//     delay: (el, i) => 50 * i
+//   }).add({
+//     targets: '.ml6',
+//     opacity: 0,
+//     duration: 2000,
+//     easing: "easeOutExpo",
+//     delay: 6000
+//   });
 
 
 //   AOS.init();
